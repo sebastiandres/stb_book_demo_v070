4b6cf39790c73d07712bcff7c109741818f2c87b
sebastiandres <sebastiandres@gmail.com>
2021-11-21 20:21:03 -0300

    Initial version with library

---
+++ book.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit_book as stb
+
+# Streamlit webpage properties
+st.set_page_config(layout="wide", page_title="Streamlit Book", page_icon="📖",)
+
+# Streamit book properties
+stb.set_book_config(path="Demo Day", button="top")
