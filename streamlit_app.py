53dd029a70bf7bc19cfee4f4abba2fbc41bb095e
sebastiandres <sebastiandres@gmail.com>
2022-02-23 18:54:43 -0300

    updating to test on share.st

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,40 @@
 import streamlit as st
 import streamlit_book as stb
+from pathlib import Path
 
-# Streamlit webpage properties
-st.set_page_config(layout="wide", page_title="Streamlit Book", page_icon="📖",)
+def main():
+       # Streamlit webpage properties
+       st.set_page_config(layout="wide", page_title="Streamlit Book", page_icon="📖",)
 
-# Streamit book properties
-save_answers = True
-stb.set_library_config(menu_title="",
-                       options=[
-                                "Intro",   
-                                "Multitest", 
-                                "To Do list", 
-                                "True or False", 
-                                "Multiple Choice", 
-                                "Single Choice",
-                                "Demo Day",
-                                "End",   
-                                ], 
-                       paths=[
-                              "pages/Intro.py", 
-                              "pages/00 Multitest", 
-                              "pages/01 To do Lists", 
-                              "pages/02 True or False",
-                              "pages/03 Multiple choice",
-                              "pages/04 Single choice",
-                              "pages/DemoDay",                             
-                              "pages/End.md", 
-                              ],
-                       icons=["tree", 
-                              "code", 
-                              "robot", 
-                              "moon", 
-                              "alarm", 
-                              "activity", 
-                              "apple",
-                              "tree",
-                              ],
-                       save_answers=save_answers,
-                       )
+       # Streamit book properties
+       save_answers = True
+       current_path = Path(__file__).parent.absolute()
+       stb.set_book_config(menu_title="streamlit_book",
+                            menu_icon="lightbulb",
+                            options=[
+                                          "What's new on v0.7.0?",   
+                                          "Core Features", 
+                                          "Multipages", 
+                                          "Answers", 
+                                          "Admin View", 
+                                          ], 
+                            paths=[
+                                          current_path / "pages/00_whats_new.py", 
+                                          current_path / "pages/01 Multitest", 
+                                          current_path / "pages/02_multipage.py",
+                                          current_path / "pages/03_answers.py",
+                                          current_path / "pages/04_admin_view.py",
+                                   ],
+                            icons=[
+                                          "code", 
+                                          "robot", 
+                                          "book", 
+                                          "pin-angle", 
+                                          "file-earmark-ruled",
+                                   ],
+                            save_answers=save_answers,
+                            )
+
+
+if __name__ == "__main__":
+       main()
